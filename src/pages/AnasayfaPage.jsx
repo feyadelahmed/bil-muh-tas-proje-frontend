bd3ec761346e31e1e3c025ed0f2bc1ee915b88ea
Feyad Elahmed <feyadelahmed@gmail.com>
2024-12-29 01:22:29 +0300

    redirects

--- src/pages/AnasayfaPage.jsx
+++ src/pages/AnasayfaPage.jsx
@@ -4,10 +4,7 @@ import { Link } from "react-router";
 export default function() {
   return (
     <>
-    <Link to="/profile">Hel</Link>
-    <Link to="/kayit">kay</Link>
-    <Link to="/giris">Gir</Link>
-    
+    لسا ما عملتها لهي الصفحة :)
     </>
   );
 }
